5a544a0d97be939e1cedcecb6e8a920131026041
Klaus Sinani <klauscfhq@protonmail.com>
2018-11-01 01:43:49 +0200

    Added `apply` module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const abs = require('./src/abs');
 const absDiff = require('./src/abs-diff');
 const add = require('./src/add');
 const and = require('./src/and');
+const apply = require('./src/apply');
 const curry = require('./src/curry');
 const or = require('./src/or');
 const sub = require('./src/sub');
@@ -13,6 +14,7 @@ module.exports = {
   absDiff,
   add,
   and,
+  apply,
   curry,
   or,
   sub,
---
+++ src/apply.js
@@ -0,0 +1,8 @@
+'use strict';
+const curry = require('./curry');
+
+function apply(x, fn) {
+  return fn(x);
+}
+
+module.exports = curry(apply);
