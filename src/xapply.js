67c56f525931e5bccb672ace8c5ecb7fc74eddf0
Klaus Sinani <klauscfhq@protonmail.com>
2018-11-04 23:58:37 +0200

    Added `xapply` module

---
+++ src/xapply.js
@@ -0,0 +1,14 @@
+'use strict';
+const apply = require('./apply');
+const curry = require('./curry');
+const decr = require('./decr');
+const lte = require('./lte');
+
+function xapply(x, y, fn) {
+  if (lte(x, 0)) {
+    return y;
+  }
+  return xapply(decr(x), apply(y, fn), fn);
+}
+
+module.exports = curry(xapply);
