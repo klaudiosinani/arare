feceb37b0b38889cd6b22e9bf60b192c96577044
Klaus Sinani <klauscfhq@protonmail.com>
2018-11-04 23:59:29 +0200

    Added `filtering` module

---
+++ src/filtering.js
@@ -0,0 +1,12 @@
+'use strict';
+const apply = require('./apply');
+const curry = require('./curry');
+
+function filtering(fn, reducer, acc, x) {
+  if (apply(x, fn)) {
+    return reducer(acc, x);
+  }
+  return acc;
+}
+
+module.exports = curry(filtering);
