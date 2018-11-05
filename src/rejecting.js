cf9756e39e0ec63c001cace96b45e2129a20b436
Klaus Sinani <klauscfhq@protonmail.com>
2018-11-05 02:15:38 +0200

    Added `rejecting` module

---
+++ src/rejecting.js
@@ -0,0 +1,12 @@
+'use strict';
+const apply = require('./apply');
+const curry = require('./curry');
+
+function rejecting(fn, reducer, acc, x) {
+  if (apply(x, fn)) {
+    return acc;
+  }
+  return reducer(acc, x);
+}
+
+module.exports = curry(rejecting);
