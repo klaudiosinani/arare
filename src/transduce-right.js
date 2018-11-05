8190844355ba1f7a61430f4f6d51ebf8cd05828f
Klaus Sinani <klauscfhq@protonmail.com>
2018-11-05 02:20:31 +0200

    Added `transduce-right` module

---
+++ src/transduce-right.js
@@ -0,0 +1,14 @@
+'use strict';
+const curry = require('./curry');
+const notArr = require('./not-arr');
+const reverse = require('./reverse');
+const transduce = require('./transduce');
+
+function transduceRight(fn, reducer, acc, xs) {
+  if (notArr(xs)) {
+    throw new TypeError('[transduceRight] Last argument must be an array');
+  }
+  return transduce(fn, reducer, acc, reverse(xs));
+}
+
+module.exports = curry(transduceRight);
