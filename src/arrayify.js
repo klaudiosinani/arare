5a4faf3e9a41cb4d4c0a0931660fa59b79ab38d9
Klaus Sinani <klauscfhq@protonmail.com>
2018-11-06 20:19:58 +0200

    Updated `arrayify` to use `copy` module on array inputs

--- src/arrayify.js
+++ src/arrayify.js
@@ -1,9 +1,10 @@
 'use strict'
+const copy = require('./copy')
 const curry = require('./curry')
 const isArr = require('./is-arr')
 
 function arrayify(x) {
-  return isArr(x) ? x : [x]
+  return isArr(x) ? copy(x) : [x]
 }
 
 module.exports = curry(arrayify)
