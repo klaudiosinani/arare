ae372a31c52adc5ae30df3a27cee16586a0c3b62
Klaus Sinani <klauscfhq@protonmail.com>
2018-11-10 23:35:51 +0200

    Minor refactoring to `min-max` module

--- src/min-max.js
+++ src/min-max.js
@@ -1,18 +1,17 @@
 'use strict'
+const append = require('./append')
+const arrayMax = require('./array-max')
+const arrayMin = require('./array-min')
 const curry = require('./curry')
-const fst = require('./fst')
 const isPair = require('./is-pair')
-const max = require('./max')
-const min = require('./min')
 const not = require('./not')
 const pair = require('./pair')
-const snd = require('./snd')
 
 function minMax(xs, x) {
   if (not(isPair(xs)))
     throw new Error('[minMax] First argument must be an array of length 2')
 
-  return pair(min(fst(xs), x), max(snd(xs), x))
+  return pair(arrayMin(append(x, xs)), arrayMax(append(x, xs)))
 }
 
 module.exports = curry(minMax)
