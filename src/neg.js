f631b090d5b734fde66d0e0a208e50fe5e82bef6
Klaus Sinani <klauscfhq@protonmail.com>
2018-11-10 23:29:25 +0200

    Minor refactoring on `neg` module

--- src/neg.js
+++ src/neg.js
@@ -1,8 +1,10 @@
 'use strict'
+const abs = require('./abs')
 const curry = require('./curry')
+const mult = require('./mult')
 
 function neg(x) {
-  return -Number(x)
+  return mult(abs(x), -1)
 }
 
 module.exports = curry(neg)
