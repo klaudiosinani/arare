712215f4a702a643acc4e715c3a9ba04e5888e13
Klaus Sinani <klauscfhq@protonmail.com>
2018-11-12 04:05:36 +0200

    Minor refactoring on `xapply` module

--- src/xapply.js
+++ src/xapply.js
@@ -4,11 +4,11 @@ const curry = require('./curry')
 const decr = require('./decr')
 const lte = require('./lte')
 
-function xapply(x, fn, y) {
+function xapply(x, y, fn) {
   if (lte(x, 0))
     return y
 
-  return xapply(decr(x), fn, apply(y, fn))
+  return xapply(decr(x), apply(y, fn), fn)
 }
 
 module.exports = curry(xapply)
