b0f428757ad0b9ef64b2a242c98aaadf32ab5cc9
Klaus Sinani <klauscfhq@protonmail.com>
2018-11-12 22:42:32 +0200

    Minor refactoring on `snd` module

--- src/snd.js
+++ src/snd.js
@@ -1,13 +1,9 @@
 'use strict'
 const curry = require('./curry')
-const last = require('./last')
-const notPair = require('./not-pair')
+const getNth = require('./get-nth')
 
 function snd(xs) {
-  if (notPair(xs))
-    throw new TypeError('[snd] Argument must be an array of length 2')
-
-  return last(xs)
+  return getNth(1, xs)
 }
 
 module.exports = curry(snd)
